a707ee09e8adeee48082c649df8809591e5ef973
Dorik (Labeeb) <ahmedlabeeb@Ahmeds-MacBook-Air.local>
2022-09-19 07:09:28 +0600

    custom header footer done

--- wp-content/themes/fictional-university-theme/functions.php
+++ wp-content/themes/fictional-university-theme/functions.php
@@ -9,12 +9,20 @@
     wp_enqueue_style - first argument takes in the CSS file name , second argument takes in the file location.
 
     get_stylesheet_uri function automatically loads the style.css file
+
+    get_theme_file_uri function allows loading custom CSS files
 -->
 
 <?php
 function university_files()
 {
-    wp_enqueue_style('university_main_styles', get_stylesheet_uri());
+    // wp_enqueue_style('university_main_styles', get_stylesheet_uri());
+    // this is the process of loading google fonts
+    wp_enqueue_style('custom-google-fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
+    // this is the process of loading font awesome
+    wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
+    wp_enqueue_style('university_main_styles', get_theme_file_uri('/build/style-index.css'));
+    wp_enqueue_style('university_extra_styles', get_theme_file_uri('/build/index.css'));
 }
 
 add_action('wp_enqueue_scripts', 'university_files');
