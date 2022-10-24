a33aee38fb0c4d2788ef3c29ca533b0d1c4cb623
Dorik (Labeeb) <ahmedlabeeb@Ahmeds-MacBook-Air.local>
2022-10-25 01:34:54 +0600

    Creating Relationships Between Content

--- wp-content/themes/fictional-university-theme/functions.php
+++ wp-content/themes/fictional-university-theme/functions.php
@@ -1,19 +1,22 @@
-<!--Using the add_action function WP allow's us to pass instruction; 
-    the first argument tells WP what type of function we are giving it,
-    the second argument takes in the type of function we want to run
-    we will need to create the custom function.
-     
-    wp_enqueue_scripts instructs WP to load CSS and JS files
-    university_files is the function we have created.
-
-    wp_enqueue_style - first argument takes in the CSS file name , second argument takes in the file location.
-
-    get_stylesheet_uri function automatically loads the style.css file
-
-    get_theme_file_uri function allows loading custom CSS files
--->
-
 <?php
+
+// Important note -  Removed all HTML Code because of errors.
+
+    // Using the add_action function WP allow's us to pass instruction; 
+    // the first argument tells WP what type of function we are giving it,
+    // the second argument takes in the type of function we want to run
+    // we will need to create the custom function.
+     
+    // wp_enqueue_scripts instructs WP to load CSS and JS files
+    // university_files is the function we have created.
+
+    // wp_enqueue_style - first argument takes in the CSS file name , second argument takes in the file location.
+
+    // get_stylesheet_uri function automatically loads the style.css file
+
+    // get_theme_file_uri function allows loading custom CSS files
+
+
 function university_files()
 { 
     // wp_enqueue_style('university_main_styles', get_stylesheet_uri());
