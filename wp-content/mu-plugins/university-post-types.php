ad7597fc61718d49558dec7e194f7aa83aae757d
Crazy Coder <labeeb.edu123@gmail.com>
2023-02-10 01:24:17 +0600

    User Roles and Permissions

--- wp-content/mu-plugins/university-post-types.php
+++ wp-content/mu-plugins/university-post-types.php
@@ -6,6 +6,13 @@
     // Registers a post type
     // First argument - pass the preferred name for the custom post type, Second argument - asociative array describing the post type.
     register_post_type('event', array(
+        // By default the capability type is set to 'post'
+
+        // We basically just wanna tell the event post type that it should not behave like a blog post for user permissions and capabilities.
+        // Set the capability_type to something unique such as event, then explicitly grant these custom capability permissions to each roles as we see fit.  
+        'capability_type' =>'event',
+        //Without this line of code we would need to create custom logic for when the above capabilities should be required
+        'map_meta_cap' => true,
         // this creates an endpoint for the custom post type
         'show_in_rest' => true,
         'supports' => array('title', 'editor', 'excerpt'),
